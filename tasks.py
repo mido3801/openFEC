70b1e1f1b97d6785322911da6568c5c7a7552b5d
Priyanka Kasireddy <pkasireddy@fec.gov>
2019-03-05 21:31:43 -0500

    Update buildpack to cflinuxfs3

--- tasks.py
+++ tasks.py
@@ -143,8 +143,8 @@ def _detect_space(repo, branch=None, yes=False):
 DEPLOY_RULES = (
     ('prod', _detect_prod),
     ('stage', lambda _, branch: branch.startswith('release')),
-    ('dev', lambda _, branch: branch == 'develop'),
-    # ('dev', lambda _, branch: branch == 'feature/create_bastion_host'),
+    # ('dev', lambda _, branch: branch == 'develop'),
+    ('dev', lambda _, branch: branch == 'feature/update-buildpack-cflinuxfs3'),
 )
 
 
